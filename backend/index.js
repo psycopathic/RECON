f6c5d03d12e71df857f4535d46d9a6e5e718807c
psycopathic <harshsrivastava2502@gmail.com>
2026-03-04 20:28:49 +0530

    going to deploy

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+dotenv.config();
 
 import authRouter from './src/routes/authRoutes.js';
 import productRouter from './src/routes/productRoutes.js';
@@ -11,19 +12,30 @@ import analyticsRouter from './src/routes/analyticsRoutes.js';
 import cors from 'cors';
 import { connectDB } from './src/lib/db.js';
 import cookieParser from 'cookie-parser';
-dotenv.config();
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.join(__dirname, '.env') });
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors(
-    {
-        origin:'http://localhost:5173',
-        credentials:true
-    }
-));
+app.use(cors({
+    origin: process.env.NODE_ENV === "production" 
+        ? process.env.CLIENT_URL 
+        : "http://localhost:5173",
+    credentials:true
+}))
 app.use(express.json({limit:"100mb"}));
 app.use(cookieParser());
+
+if(process.env.NODE_ENV === "production") {
+    const distPath = path.join(__dirname, '../frontend/dist');
+    app.use(express.static(distPath));
+}
 
 app.use('/api/auth',authRouter);
 app.use('/api/products', productRouter);
@@ -35,6 +47,17 @@ app.use('/api/analytics', analyticsRouter);
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+if(process.env.NODE_ENV === "production") {
+    const distPath = path.join(__dirname, '../frontend/dist');
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(distPath, 'index.html'));
+    });
+} else {
+    app.get('/', (req, res) => {
+        res.send('Backend is running! Frontend at http://localhost:5173')
+    });
+}
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
